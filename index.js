02fb5ee4c605400065825071716f7cb94f723011
Tham Cheong Kit <tham.cheongkit@setel.com>
2023-12-21 08:41:54 +0800

    feat(): send webhook msg

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(process.env.WEBHOOK_URL)
+(async () => {
+    const res = await fetch(
+        process.env.WEBHOOK_URL,
+        {
+            method: "POST",
+            headers: { ["Content-Type"]: "application/json" },
+            body: JSON.stringify({ content: "Hello from github workflow" })
+        }
+    )
+    const x = await res.text()
+    process.exit(0)
+})()
